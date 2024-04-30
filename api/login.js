e815e6403f26bbac7f18f9039066fba89369235b
Johann Osnaya <johann.osnaya@gmail.com>
2024-04-30 15:36:58 -0600

    Fix: express routes

--- api/login.js
+++ api/login.js
@@ -1,36 +1,6 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/user')
+const app = require('../app')
+const route = require('../routes/login')
 
-loginRouter.post('/', async (request, response) => {
-	const { username, password } = request.body
+app.use('/api/login', route)
 
-	const user = await User.findOne({ username })
-	const passwordCorrect = user === null
-		? false
-		: await bcrypt.compare(password, user.passwordHash)
-
-	if(!(user && passwordCorrect)) {
-		return response.status(400).send({
-			error: 'invalid password or username'
-		})
-	}
-
-	const userForToken = {
-		username: user.username,
-		id: user._id
-	}
-
-	const token = jwt.sign(
-		userForToken,
-		process.env.SECRET,
-		{ expiresIn: 60*60 }
-	)
-
-	response
-		.status(200)
-		.send({ token, username: user.username, name: user.name })
-})
-
-module.exports = loginRouter
+module.exports = app;
